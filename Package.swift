835f6d46d5fbfb844a9ef79fb2a8bb6067f07129
Hiroshi Kimura <muukii.app@gmail.com>
2025-12-06 20:02:21 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.1.0"),
+    .package(url: "https://github.com/siteline/swiftui-introspect", "1.3.0"..<"27.0.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
